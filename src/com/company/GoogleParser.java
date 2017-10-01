2146af7562a630dadfaa24b9a6f4c563108231cf
DonnaTCT <ttt_donna_tong@hotmail.com>
2017-09-30 21:57:45 -0700

    Parser

---
+++ src/com/company/GoogleParser.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class GoogleParser {
+
+   // https://maps.googleapis.com/maps/api/directions/json?parameters
+   // https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk
+    // API-Key: AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk
+
+    private String googleurl = "https://maps.googleapis.com/maps/api/directions/json?";
+    private String origin = "origin=";
+    private String dst = "&destination=";
+    private String key = "&key=AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk";
+
+
+    public void parseResponce(String json){
+        try {
+            JSONObject resp = new JSONObject(json);
+            JSONArray routes = resp.getJSONArray("routes");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ src/test/GoogleParserTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class GoogleParserTest {
+
+    @Test
+    public void testParser(){
+        
+    }
+
+}
+
