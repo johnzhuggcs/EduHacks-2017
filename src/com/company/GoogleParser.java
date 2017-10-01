1a6f28fd4f3dd412384303a4e18131d9641f4f56
DonnaTCT <ttt_donna_tong@hotmail.com>
2017-09-30 23:25:08 -0700

    Parser

--- src/com/company/GoogleParser.java
+++ src/com/company/GoogleParser.java
@@ -11,46 +11,8 @@ import java.nio.charset.Charset;
 
 public class GoogleParser {
 
-   // https://maps.googleapis.com/maps/api/directions/json?parameters
-   // https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk
-    // API-Key: AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk
+    public GoogleParser(){
 
-    private String googleurl = "https://maps.googleapis.com/maps/api/directions/json?";
-    private String origin = "origin=";
-    private String dst = "&destination=";
-    private String key = "&key=AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk";
-
-    public GoogleParser(String origin, String dst){
-        this.origin += origin;
-        this.dst += dst;
-        googleurl+= this.origin + this.dst + key;
-        //System.out.println(googleurl);
-    }
-
-    public String setConnection(){
-        try {
-            URL url = new URL(googleurl);
-            InputStream is = null;
-            is = url.openStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            //System.out.println(jsonText);
-            return jsonText;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
     }
 
     public void parseResponce(String json){
