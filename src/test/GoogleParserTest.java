1baafd3bc4bd81b39ca25d0a54a561eacf2edc19
DonnaTCT <ttt_donna_tong@hotmail.com>
2017-09-30 22:05:38 -0700

    Parser

--- src/test/GoogleParserTest.java
+++ src/test/GoogleParserTest.java
@@ -1,12 +1,24 @@
 package test;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class GoogleParserTest {
 
     @Test
     public void testParser(){
-        
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Chicago,IL&destination=Los+Angeles,CA&waypoints=Joplin,MO|Oklahoma+City,OK&key=AIzaSyDGfTWMXhGEkmUGYn9E5ZFVO1J7krYEHvk");
+            url.openConnection();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
